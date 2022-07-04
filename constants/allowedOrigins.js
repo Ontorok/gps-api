1cff3e4d483c755b84b16698774ff210f387a269
Ontorok <nasir.amath07@gmail.com>
2022-07-05 00:09:41 +0600

    origin add for  home

--- constants/allowedOrigins.js
+++ constants/allowedOrigins.js
@@ -6,7 +6,6 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://keen-mclean-9c0748.netlify.app",
   "https://gps-scheluder-v1.herokuapp.com",
-  "http://54.203.84.201",
 ];
 
 module.exports = allowedOrigins;
