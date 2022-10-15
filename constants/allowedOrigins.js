553fa8b2d9684e265ce05f7e522882a377509a3c
Ontorok <42499277+Ontorok@users.noreply.github.com>
2022-10-15 11:24:51 +0600

    Update allowedOrigins.js

--- constants/allowedOrigins.js
+++ constants/allowedOrigins.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://keen-mclean-9c0748.netlify.app",
   "https://gps-scheluder-v1.herokuapp.com",
+  "https://18.205.162.210"
 ];
 
 module.exports = allowedOrigins;
